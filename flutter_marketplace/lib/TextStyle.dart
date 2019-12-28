07f0dfb2fea54d7281fc760913aeeec6e9b6ffd0
bootgem2 <bootgem2@gmail.com>
2019-12-28 17:13:16 +0700

    add new file to collect all TextStyle that needed

---
+++ flutter_marketplace/lib/TextStyle.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+final TextStyle fadedTextStyle = TextStyle(
+  fontSize: 16.0,
+  fontWeight: FontWeight.bold,
+  color: Color(0x99ffffff),
+);
+
+final TextStyle whiteHeadingTextStyle= TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.bold,
+  color: Color(0xffffffff),
+);
+
+final TextStyle categoryTextStyle = TextStyle(
+  fontSize: 14.0,
+  fontWeight: FontWeight.bold,
+  color:Colors.white,
+);
+
+final TextStyle selectedCategoryTextStyle = categoryTextStyle.copyWith(
+  color: Colors.red[300],
+);
+
+final TextStyle eventTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  color:Colors.black,
+);
+
+final TextStyle eventTitleWhiteTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+final TextStyle  eventLocationTextStyle = TextStyle(
+  fontSize: 20.0,
+  color:Colors.black,
+);
+
+final TextStyle guestTextStyle = TextStyle(
+  fontSize: 16.0,
+  fontWeight: FontWeight.w800,
+);
+
+final TextStyle punchLine1TextStyle = TextStyle(
+  fontSize: 28.0,
+  fontWeight: FontWeight.w800,
+  color: Colors.red[300],
+);
+
+final TextStyle punchLine2TextStyle = punchLine1TextStyle.copyWith(
+  color: Colors.black,
+);
